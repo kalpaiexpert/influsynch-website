0c267b1dd4d81f5180875a28200be99dcfd6e3b2
kalpaiexpert <v.c.thakkar86@gmail.com>
2025-06-06 16:25:26 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('menu-toggle').addEventListener('click', function() {
+    const mobileMenu = document.getElementById('mobile-menu');
+    mobileMenu.classList.toggle('hidden');
+    mobileMenu.classList.toggle('active');
+});
