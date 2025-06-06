eecb029f6bd4fb96fd47b1b0e248ada60d6495e2
kalpaiexpert <v.c.thakkar86@gmail.com>
2025-06-06 19:02:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 document.getElementById('menu-toggle').addEventListener('click', function() {
     const mobileMenu = document.getElementById('mobile-menu');
     mobileMenu.classList.toggle('hidden');
-    mobileMenu.classList.toggle('active');
 });
+
+function toggleDropdown(dropdownId) {
+    const dropdown = document.getElementById(dropdownId);
+    dropdown.classList.toggle('hidden');
+}
